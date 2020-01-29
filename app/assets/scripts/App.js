b1760b8addf750761bc9bc63f93404a01f6021d3
kuan lyu <46909761+kuanlv@users.noreply.github.com>
2020-01-29 09:14:33 -0500

    completed desktop and moblie

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,13 @@
-import '../styles/styles.css';
+import '../styles/styles.css'; 
+import MobileMenu from './modules/MobileMenu'; 
 
 if (module.hot) {
     module.hot.accept();
 }
+
+let mobileMenu = new MobileMenu();
+
+
+
+
+
