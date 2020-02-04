f9bb6a72be9ef69aca9fec40099a0935b645380f
kuan lyu <46909761+kuanlv@users.noreply.github.com>
2020-02-04 11:08:01 -0500

    sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,12 @@
 import '../styles/styles.css'; 
 import MobileMenu from './modules/MobileMenu'; 
 import RevealOnScroll from './modules/RealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
-
+let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+
 let mobileMenu = new MobileMenu();
 if (module.hot) {
     module.hot.accept();
